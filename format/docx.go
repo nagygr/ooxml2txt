f3e24b3302797e52b9fc8669794607831fb9eed1
Gergely Nagy <nagygr@gmail.com>
2022-04-20 20:30:12 +0200

    [ADD] Docx format.

---
+++ format/docx.go
@@ -0,0 +1,174 @@
+package format
+
+import (
+	"archive/zip"
+	"errors"
+	"github.com/nagygr/ooxml2txt/archive"
+	"io"
+	"io/ioutil"
+	"strings"
+)
+
+type Docx struct {
+	zipReader archive.ZipData
+	content   string
+	links     string
+	headers   map[string]string
+	footers   map[string]string
+}
+
+func MakeDocx(path string) (*Docx, error) {
+	reader, err := archive.MakeZipFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := readText(reader.Files())
+	if err != nil {
+		return nil, err
+	}
+
+	links, err := readLinks(reader.Files())
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+	 * Error not handled: it is ok to not find headers/footers
+	 */
+	headers, footers, _ := readHeaderFooter(reader.Files())
+
+	return &Docx {
+		zipReader: reader,
+		content: content,
+		links: links,
+		headers: headers,
+		footers: footers}, nil
+}
+
+func (d *Docx) Text() string {
+	return d.content
+}
+
+func readHeaderFooter(files []*zip.File) (headerText map[string]string, footerText map[string]string, err error) {
+	h, f, err := retrieveHeaderFooterDoc(files)
+
+	if err != nil {
+		return map[string]string{}, map[string]string{}, err
+	}
+
+	headerText, err = buildHeaderFooter(h)
+	if err != nil {
+		return map[string]string{}, map[string]string{}, err
+	}
+
+	footerText, err = buildHeaderFooter(f)
+	if err != nil {
+		return map[string]string{}, map[string]string{}, err
+	}
+
+	return headerText, footerText, err
+}
+
+func buildHeaderFooter(headerFooter []*zip.File) (map[string]string, error) {
+	headerFooterText := make(map[string]string)
+
+	for _, element := range headerFooter {
+		documentReader, err := element.Open()
+		if err != nil {
+			return map[string]string{}, err
+		}
+
+		text, err := wordDocToString(documentReader)
+		if err != nil {
+			return map[string]string{}, err
+		}
+
+		headerFooterText[element.Name] = text
+	}
+
+	return headerFooterText, nil
+}
+
+func readText(files []*zip.File) (text string, err error) {
+	var documentFile *zip.File
+	documentFile, err = retrieveWordDoc(files)
+	if err != nil {
+		return text, err
+	}
+
+	var documentReader io.ReadCloser
+	documentReader, err = documentFile.Open()
+	if err != nil {
+		return text, err
+	}
+
+	text, err = wordDocToString(documentReader)
+	return
+}
+
+func readLinks(files []*zip.File) (text string, err error) {
+	var documentFile *zip.File
+	documentFile, err = retrieveLinkDoc(files)
+	if err != nil {
+		return text, err
+	}
+
+	var documentReader io.ReadCloser
+	documentReader, err = documentFile.Open()
+	if err != nil {
+		return text, err
+	}
+
+	text, err = wordDocToString(documentReader)
+	return
+}
+
+func wordDocToString(reader io.Reader) (string, error) {
+	b, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func retrieveWordDoc(files []*zip.File) (file *zip.File, err error) {
+	for _, f := range files {
+		if f.Name == "word/document.xml" {
+			file = f
+		}
+	}
+	if file == nil {
+		err = errors.New("document.xml file not found")
+	}
+	return
+}
+
+func retrieveLinkDoc(files []*zip.File) (file *zip.File, err error) {
+	for _, f := range files {
+		if f.Name == "word/_rels/document.xml.rels" {
+			file = f
+		}
+	}
+	if file == nil {
+		err = errors.New("document.xml.rels file not found")
+	}
+	return
+}
+
+func retrieveHeaderFooterDoc(files []*zip.File) (headers []*zip.File, footers []*zip.File, err error) {
+	for _, f := range files {
+
+		if strings.Contains(f.Name, "header") {
+			headers = append(headers, f)
+		}
+		if strings.Contains(f.Name, "footer") {
+			footers = append(footers, f)
+		}
+	}
+	if len(headers) == 0 && len(footers) == 0 {
+		err = errors.New("headers[1-3].xml file not found and footers[1-3].xml file not found")
+	}
+	return
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/nagygr/ooxml2txt/archive"
+	"github.com/nagygr/ooxml2txt/format"
 	"log"
 )
 
@@ -14,4 +15,12 @@ func main() {
 	}
 
 	fmt.Printf("Number of files: %d\n", len(zipfile.Files()))
+
+	docxfile, err := format.MakeDocx("test_data/example.docx")
+
+	if err != nil {
+		log.Fatal("Couldn't open docx file: %s", err)
+	}
+
+	fmt.Printf("Text: %s\n", docxfile.Text())
 }
