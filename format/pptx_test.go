09e2440c6eac9842c154ecdad9d781037df41c62
Gergely Nagy <nagygr@gmail.com>
2022-04-23 23:52:10 +0200

    [ADD] Tests for pptx and xlsx.

---
+++ format/pptx_test.go
@@ -0,0 +1,47 @@
+package format
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestMakingPptxGoodPath(t *testing.T) {
+	path := "../test_data/example.pptx"
+	_, err := MakePptx(path)
+
+	if err != nil {
+		t.Errorf("Expected to open %s successfully", path)
+	}
+}
+
+func TestMakingPptxBadPath(t *testing.T) {
+	path := "../test_data/wrong_example.pptx"
+	_, err := MakePptx(path)
+
+	if err == nil {
+		t.Errorf("Expected to fail to open %s successfully", path)
+	}
+}
+
+func TestReadingPptxText(t *testing.T) {
+	path := "../test_data/example.pptx"
+	ppt, err := MakePptx(path)
+
+	if err != nil {
+		t.Errorf("Expected to open %s successfully", path)
+	}
+
+	if len(ppt.Text) != 2 {
+		t.Errorf("Expected to have 2 slides, has: %d", len(ppt.Text))
+	}
+
+	text := "Aalborg"
+	if !strings.HasPrefix(ppt.Text[0], text) {
+		t.Errorf("Expected the text to start with: \"%s\"", text)
+	}
+
+	text = "Limfjordsbroen"
+	if !strings.HasSuffix(strings.TrimSpace(ppt.Text[1]), text) {
+		t.Errorf("Expected the text to end with: \"%s\"", text)
+	}
+}
---
+++ format/xlsx_test.go
@@ -0,0 +1,48 @@
+package format
+
+import (
+	_ "fmt"
+	"strings"
+	"testing"
+)
+
+func TestMakingXlsxGoodPath(t *testing.T) {
+	path := "../test_data/example.xlsx"
+	_, err := MakeXlsx(path)
+
+	if err != nil {
+		t.Errorf("Expected to open %s successfully", path)
+	}
+}
+
+func TestMakingXlsxBadPath(t *testing.T) {
+	path := "../test_data/wrong_example.xlsx"
+	_, err := MakeXlsx(path)
+
+	if err == nil {
+		t.Errorf("Expected to fail to open %s successfully", path)
+	}
+}
+
+func TestReadingXlsxText(t *testing.T) {
+	path := "../test_data/example.xlsx"
+	xls, err := MakeXlsx(path)
+
+	if err != nil {
+		t.Errorf("Expected to open %s successfully", path)
+	}
+	if len(xls.Text) != 3 {
+		t.Errorf("Expected to have 3 strings, has: %d", len(xls.Text))
+	}
+
+	text := "Odense"
+	if !strings.HasPrefix(xls.Text[0], text) {
+		t.Errorf("Expected the text to start with: \"%s\"", text)
+	}
+
+	text = "Copenhagen"
+	if !strings.HasSuffix(strings.TrimSpace(xls.Text[2]), text) {
+		t.Errorf("Expected the text to end with: \"%s\"", text)
+	}
+}
+
