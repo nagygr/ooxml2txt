8ba0d34a205a221854614cbacd7057b21a09f137
Gergely Nagy <nagygr@gmail.com>
2022-05-28 08:28:40 +0200

    [ADD] Add URL download functionality to Zip files and using it with Docx.

--- internal/archive/zipfile.go
+++ internal/archive/zipfile.go
@@ -8,14 +8,50 @@ package archive
 
 import (
 	"archive/zip"
+	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 )
 
+type zipReader interface {
+	Files() []*zip.File
+	Close() error
+}
+
+type zipFileReader struct {
+	reader *zip.ReadCloser
+}
+
+var _ zipReader = (*zipFileReader)(nil)
+
+func (r *zipFileReader) Files() []*zip.File {
+	return r.reader.File
+}
+
+func (r *zipFileReader) Close() error {
+	return r.reader.Close()
+}
+
+type zipUrlReader struct {
+	reader *zip.Reader
+}
+
+var _ zipReader = (*zipUrlReader)(nil)
+
+func (r *zipUrlReader) Files() []*zip.File {
+	return r.reader.File
+}
+
+func (r *zipUrlReader) Close() error {
+	return nil
+}
+
 // ZipFile is an implementation of the ZipData interface for actual zip files.
 type ZipFile struct {
-	data *zip.ReadCloser
+	data zipReader
 }
 
 // MakeZipFile creates a ZipFile for an actual zip file given by its path.
@@ -26,12 +62,33 @@ func MakeZipFile(path string) (*ZipFile, error) {
 		return nil, err
 	}
 
-	return &ZipFile{data: reader}, nil
+	return &ZipFile{data: &zipFileReader{reader}}, nil
+}
+
+// MakeZipFileFromUrl creates a ZipFile from a URL
+func MakeZipFileFromUrl(url string) (*ZipFile, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
+	if err != nil {
+		return nil, err
+	}
+
+	return &ZipFile{data: &zipUrlReader{reader}}, nil
 }
 
 // Files returns the files inside the zip archive.
 func (z *ZipFile) Files() []*zip.File {
-	return z.data.File
+	return z.data.Files()
 }
 
 // close closes the zip reader.
@@ -41,7 +98,7 @@ func (z *ZipFile) close() error {
 
 // FileByName finds the file with the given name or returns an error.
 func (z *ZipFile) FileByName(name string) (file *zip.File, err error) {
-	for _, f := range z.data.File {
+	for _, f := range z.data.Files() {
 		if f.Name == name {
 			file = f
 			break
@@ -59,7 +116,7 @@ func (z *ZipFile) FileByName(name string) (file *zip.File, err error) {
 // FilesByName finds all the files containing the given substring or
 // return an error.
 func (z *ZipFile) FilesByName(substring string) (files []*zip.File, err error) {
-	for _, f := range z.data.File {
+	for _, f := range z.data.Files() {
 		if strings.Contains(f.Name, substring) {
 			files = append(files, f)
 		}
--- pkg/format/docx_test.go
+++ pkg/format/docx_test.go
@@ -87,3 +87,58 @@ func TestMissingDocumentXml(t *testing.T) {
 		t.Errorf("Expected to get an error due to the missing document.xml.")
 	}
 }
+
+func TestReadingDocxTextFromUrl(t *testing.T) {
+	url := "https://github.com/nagygr/ooxml2txt/raw/main/test_data/example.docx"
+	doc, err := MakeDocxFromUrl(url)
+
+	if err != nil {
+		t.Fatalf("Expected to open %s successfully", url)
+	}
+
+	text := "The largest city"
+	if !strings.HasPrefix(doc.Text, text) {
+		t.Errorf("Expected the text to start with: \"%s\"", text)
+	}
+
+	text = "tourism in Jutland."
+	if !strings.HasSuffix(doc.Text, text) {
+		t.Errorf("Expected the text to end with: \"%s\"", text)
+	}
+
+	if len(doc.Links) != 7 {
+		t.Errorf("Expected to have 7 links, has: %d", len(doc.Links))
+	} else {
+		linkFragment := "Denmark_Region"
+		if !strings.Contains(doc.Links[0], linkFragment) {
+			t.Errorf("First link (%s) expected to contain: %s", doc.Links[0], linkFragment)
+		}
+	}
+
+	if len(doc.Footnotes) != 1 {
+		t.Errorf("Expected to have one footnote, has %d", len(doc.Footnotes))
+	} else {
+		footnote := "This is a footnote."
+		if doc.Footnotes[0] != footnote {
+			t.Errorf("Expected the first footnote to be: %s, was: %s", footnote, doc.Footnotes[0])
+		}
+	}
+
+	if len(doc.Headers) != 1 {
+		t.Errorf("Expected to have one header, has %d", len(doc.Headers))
+	} else {
+		header := "Header text"
+		if doc.Headers[0] != header {
+			t.Errorf("Expected the first header to be: %s, was: %s", header, doc.Headers[0])
+		}
+	}
+
+	if len(doc.Footers) != 1 {
+		t.Errorf("Expected to have one footer, has %d", len(doc.Footers))
+	} else {
+		footer := "Footer text"
+		if doc.Footers[0] != footer {
+			t.Errorf("Expected the first footer to be: %s, was: %s", footer, doc.Footers[0])
+		}
+	}
+}
